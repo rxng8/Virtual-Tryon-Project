e57c3c45d8f2cf7c930c6716123f0fd743c845a2
Alex Nguyen <alexvn.edu@gmail.com>
2020-12-21 01:25:47 -0500

    add tenplate code

---
+++ core/human_parsing.py
@@ -0,0 +1,17 @@
+# %%
+
+import sys
+import matplotlib.pyplot as plt
+from pathlib import Path
+from typing import List, Dict, Tuple
+import numpy as np
+import pandas as pd
+from PIL import Image
+import math
+import os
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import cv2
+
+
