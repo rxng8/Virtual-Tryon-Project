8e9d5ad39bb0067c48f0156ccda4ab789bf9a997
Alex Nguyen <alexvn.edu@gmail.com>
2020-12-25 01:32:10 -0500

    add utilities

---
+++ core/utils.py
@@ -0,0 +1,45 @@
+import sys
+import matplotlib.pyplot as plt
+from pathlib import Path
+from typing import List, Dict, Tuple
+import numpy as np
+import pandas as pd
+from PIL import Image
+import math
+import os
+import re
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import cv2
+
+def conv(batch_input, out_channels):
+    return tf.keras.layers.Conv2D(
+        filters=out_channels, 
+        kernel_size=(4, 4), 
+        activation='relu', 
+        padding='same'
+    )(batch_input)
+
+def max_pool(batch_input):
+    return tf.keras.layers.MaxPooling2D(
+        pool_size=(2, 2),
+        padding='same'
+    ) (batch_input)
+
+def final_deconv(batch_input, out_channels):
+    return tf.keras.layers.Conv2DTranspose(
+        out_channels, 4, strides=2,
+        padding='same',
+        activation='sigmoid'
+    ) (batch_input)
+
+def deconv(batch_input, out_channels):
+    return tf.keras.layers.Conv2DTranspose(
+        out_channels, 4, strides=2,
+        padding='same',
+        activation='relu'
+    ) (batch_input)
+
+def upsampling(batch_input):
+    pass
