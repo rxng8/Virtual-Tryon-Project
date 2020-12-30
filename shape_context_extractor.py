8c54973f520ff32a45166b53db83a8a278e31cc2
Alex Nguyen <alexvn.edu@gmail.com>
2020-12-30 16:49:20 -0500

    add shape context descriptor

---
+++ shape_context_extractor.py
@@ -0,0 +1,14 @@
+"""
+See this:
+    https://www.youtube.com/watch?v=m3rK3gx0tZo
+    https://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html
+    https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/shape/sc_digits.html
+    https://kyamagu.github.io/mexopencv/matlab/ShapeContextDistanceExtractor.html
+    https://deeplearning.lipingyang.org/shape-context-resources/
+    https://github.com/creotiv/Python-Shape-Context
+    https://medium.com/machine-learning-world/shape-context-descriptor-and-fast-characters-recognition-c031eac726f9
+    https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/shape/belongie-pami02.pdf
+Visualization:
+    https://www.youtube.com/watch?v=qJieWnkl9gQ
+
+"""
