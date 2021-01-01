35458588457ac6658e01d9ee5a9fcf844b319487
Alex Nguyen <alexvn.edu@gmail.com>
2021-01-01 14:36:45 -0500

    refactoring

---
+++ core/copy.py
@@ -0,0 +1,30 @@
+
+from .lip_dataset import *
+from shutil import copyfile
+
+train_path_raw, test_path_raw = get_data_path_raw()
+
+# Copy the segmentation model to input folder
+r_str = r"\/.*\.jpg$"
+
+for i, line in enumerate(train_path_raw):
+    # each line is 2 links, one is the person, 1 ius the cloth
+    # copy only th cloth to the input folder to segment.
+    url = line[0]
+    name_list = re.findall(r_str, url)
+    if len(name_list) == 0:
+        print(url)
+    else:
+        name = name_list[0][1:]
+        copyfile(DATASET_SRC / url, Path("./reference/inputs") / name)
+
+for i, line in enumerate(test_path_raw):
+    # each line is 2 links, one is the person, 1 ius the cloth
+    # copy only th cloth to the input folder to segment.
+    url = line[0]
+    name_list = re.findall(r_str, url)
+    if len(name_list) == 0:
+        print(url)
+    else:
+        name = name_list[0][1:]
+        copyfile(DATASET_SRC / url, Path("./reference/inputs") / name)
