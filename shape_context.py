693f1e5e6f8f8a9775d0b46c56694395a999ac6c
Alex Nguyen <alexvn.edu@gmail.com>
2020-12-31 23:05:09 -0500

    more on shape context

--- shape_context.py
+++ shape_context.py
@@ -12,6 +12,8 @@ from scipy.spatial.distance import cdist, cosine
 from scipy.optimize import linear_sum_assignment
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from IPython import display
+import time
 
 from core.utils import *
 
@@ -238,7 +240,49 @@ def process_contour_actual(ori_img, threshold=0.92):
     show_img(mm)
     return contours, hierarchy
 
+def mask_background(img, threshold=0.992):
+    # Expect an unprocessed cv2 img 3d. 3 channels. Image of the cloth. BGR
+    origray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    origray = np.asarray(origray)
+    origray = origray.astype('float32') / 255.0
+    # show_img(origray)
+    origray[origray > threshold] = 0
+    origray *= 255.0
+    origray = origray.astype(np.uint8)
+    return origray
 
+def draw_point(img, points):
+    # Expect np.ndarray img shape h x w x 3. Range 0 - 255
+    mx = np.max(img)
+    new_img = img.copy()
+    zeros = np.zeros(new_img.shape)
+    for [x_i, y_i] in points:
+        if mx > 1:
+            new_img[y_i-1:y_i+1, x_i-1:x_i+1, :] = (255, 0, 0)
+            zeros[y_i-1:y_i+1, x_i-1:x_i+1, :] = (255, 0, 0)
+        else:
+            new_img[y_i-1:y_i+1, x_i-1:x_i+1, :] = 1
+            zeros[y_i-1:y_i+1, x_i-1:x_i+1, :] = 1
+    return zeros, new_img
+
+def get_points(cnts, simpleto=100):
+    # Expect contours got from cv2 contours
+    mx = -1
+    i_mx = -1
+
+    for i, cnt in enumerate(cnts):
+        if cnt.shape[0] > mx:
+            i_mx = i
+            mx = cnt.shape[0]
+    # mx representing the maximum number of points in each vector in 
+    # the contour matrix.
+    # step = (mx // simpleto) + 1
+    step = mx // simpleto
+
+    points = np.asarray(cnts[i_mx]).reshape(-1, 2)
+    # points = [points[i % points.shape[0]].tolist() for i in range(0, points.shape[0] + simpleto, step)][:simpleto]
+    points = [points[i].tolist() for i in range(0, points.shape[0], step)][:simpleto]
+    return np.asarray(points)
 
 # %%
 # Test for original clothes
@@ -265,17 +309,11 @@ plt.imshow(mm)
 
 # %%
 test = cv2.imread(orig_mask, 1)
-cnts, hierarchy = process_contour(test)
+cnts, hierarchy = process_contour_cloth(test)
 # mm = cv2.drawContours(tf.zeros_like(test).numpy(), cnts, -1, (255,255,255), 3)
 # plt.imshow(mm)
-# %%
-
 contours[8].shape
-
-# %%
-
 hierarchy.shape
-
 
 # %%
 
@@ -284,62 +322,109 @@ hierarchy.shape
 cloth_mask_path = "./dataset/lip_mpv_dataset/preprocessed/clothing_mask/ZX121D004/ZX121D004-C11@10=cloth_front.jpg"
 mask_path = "./dataset/lip_mpv_dataset/MPV_192_256/ZX121D004/ZX121D004-C11@10=cloth_front.jpg"
 
+# cloth_mask_path = "./dataset/lip_mpv_dataset/preprocessed/clothing_mask/ZX121EA0A/ZX121EA0A-N11@10=cloth_front.jpg"
+# mask_path = "./dataset/lip_mpv_dataset/MPV_192_256/ZX121EA0A/ZX121EA0A-N11@10=cloth_front.jpg"
+
 cloth = cv2.imread(mask_path, 1)
 cnts_cloth, hierarchy = process_contour_cloth(cloth)
 
 img = cv2.imread(cloth_mask_path, 0)
 cnts_pred, hierarchy = process_contour_actual(img)
 
-# %%
-def get_points(cnts, simpleto=100):
-    # Expect contours got from cv2 contours
-    mx = -1
-    i_mx = -1
-
-    for i, cnt in enumerate(cnts):
-        if cnt.shape[0] > mx:
-            i_mx = i
-            mx = cnt.shape[0]
-
-    step = mx // simpleto
-
-    points = np.asarray(cnts[i_mx]).reshape(-1, 2)
-    points = [points[i].tolist() for i in range(0, points.shape[0], step)][:simpleto]
-    return np.asarray(points)
 
 # %%
 
-points_cloth = get_points(cnts_cloth)
-points_pred = get_points(cnts_pred)
+points_cloth = get_points(cnts_cloth, simpleto=50) # 100 x 2
+points_pred = get_points(cnts_pred, simpleto=50) # 100 x 2
+
+# Draw points
+points_only, abc = draw_point(np.asarray(mask_background(cloth)), points_cloth)
+show_img(points_only / 255.0)
+
+# Draw points
+points_only, abc = draw_point(np.asarray(mask_background(img)), points_pred)
+show_img(points_only / 255.0)
 
 # %%
+
 computer = ShapeContext()
-descriptor1 = computer.compute(points_cloth)
-descriptor2 = computer.compute(points_pred)
-
-# cost_matrix = computer.cost_by_paper(descriptor1, descriptor2)
+descriptor1 = computer.compute(points_cloth) # 100 x 60
+descriptor2 = computer.compute(points_pred) # 100 x 60
 
 total, indexes = computer.diff(descriptor1, descriptor2)
 
-
 # %%
+# Test point
 cnt = 0
 import collections
 hs = collections.defaultdict(int)
+source = []
+target = []
 for p1, p2 in indexes:
-    # print(f"{p1}, {p2}")
+    source.append(points_cloth[p1].tolist())
+    target.append(points_pred[p2].tolist())
     hs[p1] += 1
     hs[p2] += 1
     cnt += 1
 
-print(cnt)
+print(f"Number of matching pairs of points: {cnt}")
 
 for k, v in hs.items():
     if v > 2:
         print(k)
-print(len(hs.keys()))
+print(f"Number of unique indices: {len(hs.keys())}")
+
+# %%
+
+# See matching point
+
+for i, ([sx, sy], [tx, ty]) in enumerate(zip(source, target)):
+    display.clear_output(wait=True)
+    print(f"[{sx}, {sy}] -- [{tx}, {ty}]")
+
+    # Draw points
+    points_only, abc = draw_point(np.asarray(mask_background(cloth)), source[:i + 1])
+    show_img(points_only / 255.0)
+
+    # Draw points
+    points_only, abc = draw_point(np.asarray(mask_background(img)), target[:i + 1])
+    show_img(points_only / 255.0)
+
+    time.sleep(0.1)
 
 
+# %%
 
+tps = cv2.createThinPlateSplineShapeTransformer()
+
+# Cartesian metrics
+source = np.asarray(source, dtype=np.int32)
+target = np.asarray(target, dtype=np.int32)
+
+# source = np.asarray([[0, 0], [50, 50], [256, 33]], dtype=np.int32) # 256 x 192
+# target = np.asarray([[5, 5], [45, 50], [256, 33]], dtype=np.int32) # 256 x 192
+
+source = source.reshape(-1, len(source),2)
+target = target.reshape(-1, len(target),2)
+
+matches = list()
+for i in range(0,len(source[0])):
+    matches.append(cv2.DMatch(i,i,0))
+
+tps.estimateTransformation(target, source, matches)
+
+# %%
+prep_cloth = mask_background(cloth)
+new_img = tps.warpImage(prep_cloth)
+
+# %%
+
+show_img(cloth)
+show_img(prep_cloth)
+show_img(img)
+show_img(new_img)
+
+
+# %%
 
 
