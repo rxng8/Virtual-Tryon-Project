34e722edbf8c69fcf92e7864897aee6cfaf0fdbb
Alex Nguyen <alexvn.edu@gmail.com>
2021-01-06 00:08:53 -0500

    add some reference

---
+++ human_parsing_notebook_3.py
@@ -0,0 +1,18 @@
+"""
+First read this: about PSP Net: 
+    https://arxiv.org/pdf/1612.01105.pdf
+
+You can look through this paper also, it's about Deeplab and ASPP Net:
+    https://arxiv.org/pdf/1606.00915.pdf
+
+This file is the implementation of this paper, which is about CE2P Net:
+    https://arxiv.org/pdf/1809.05996.pdf
+
+Then read this article that talks about Augmented-CE2P Net:
+    https://arxiv.org/pdf/1910.09777.pdf
+
+Lovasz Softmax loss:
+    https://arxiv.org/pdf/1705.08790.pdf
+
+"""
+
