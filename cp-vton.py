e98e318f2682e8a9f5249f7bff9d912178821700
Alex Nguyen <alexvn.edu@gmail.com>
2021-01-06 12:42:49 -0500

    add doc

---
+++ cp-vton.py
@@ -0,0 +1,21 @@
+# %%
+
+"""
+"Finally, the experiments with existing methods reveal
+that warped clothing is often severely distorted. We could
+not clearly determine the reason, but can conclude that the
+estimation of the TPS parameters needs regularization."
+    From CP-VTON+ paper:
+    https://minar09.github.io/cpvtonplus/cvprw20_cpvtonplus.pdf
+
+Reference also thí paper CP-VTON:
+    https://arxiv.org/pdf/1807.07688.pdf
+
+The preprocessed dataset is download here:
+    https://github.com/sergeywong/cp-vton
+"""
+
+
+# %%
+
+# %%
